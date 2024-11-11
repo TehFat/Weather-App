dec417afd4b47901fd738f588efebeb91dae4776
Fatima <174022648+TehFat@users.noreply.github.com>
2024-11-11 10:37:18 +0100

    weather app

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiKey = "42e09bf5ce605e2369925b0913eee67e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+    if(response.status === 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+
+    } else {
+
+    
+    var data = await response.json();
+
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".weather").style.display = "block";
+     document.querySelector(".error").style.display = "none";
+    } 
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+checkWeather();
